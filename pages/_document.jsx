a73f5c6a54710b62d8efc5b7ad61968dac133c60
Jake <jacob@liveineverynow.com>
2022-08-05 16:50:41 -0400

    mui

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,12 +5,12 @@ class MyDocument extends Document {
     return (
       <Html lang="en-us">
         <Head>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
             <meta charSet="utf-8" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <link rel="shortcut icon" type="image/png" href="logo.png" />
             <meta name="robots" content="index, follow" />
-            <link rel="stylesheet" href="https://use.typekit.net/wzt4vsj.css"></link>
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+            <link rel="stylesheet" href="https://use.typekit.net/wzt4vsj.css" />
         </Head>
         <body>
           <Main />
