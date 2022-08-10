3d7e914513899b68e7193308ad705186a7afac24
Jake <jacob@liveineverynow.com>
2022-08-10 11:00:43 -0400

    Update LinkCard.js
    link card 1

--- components/LinkCard.js
+++ components/LinkCard.js
@@ -1,26 +1,25 @@
-import React from "react"
-import {faDiscord, faYoutube, faApple, faInstagram, faTwitter, faTiktok, faSpotify, faSoundcloud, faDeezer, faAmazon, faNapster, faTwitch, faPatreon, faFigma, faGithub,} from '@fortawesome/free-brands-svg-icons'
-import { faCupTogo } from '@fortawesome/fontawesome-svg-core';
-import Link from "next/link";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function LinkCard(props){
-
-    return(
-        <Link passHref href={props.href} className="hover:cursor-pointer">
-                    <div className="py-4 pl-4 pr-4 w-full flex-grow text-2xl flex group hover:cursor-pointer hover:bg-neutral-100 justify-between">
-                    
-                        <div className="flex items-center">
-                            <div className="">
-                                {/* <FontAwesomeIcon icon={props.fai} className="text-3xl"/> */}
-                            </div>
-                            <div className="flex space-x-3 items-center">
-                                <FontAwesomeIcon icon={props.fai} className="w-8 align-middle justify-center text-2xl"/>
-                                <p>{props.label}</p>
-                            </div>
-                        </div>
-                        <span className="material-icons text-2xl">{props.mui}</span>
-                    </div>
-                </Link>
-    )
+export default function LinkCard(props) {
+  return (
+    <Link passHref href={props.href} className="hover:cursor-pointer">
+      <div className="py-4 pl-4 pr-4 w-full flex-grow text-2xl flex group hover:cursor-pointer hover:bg-neutral-100 justify-between">
+        <div className="flex items-center">
+          <div className="">
+            {/* <FontAwesomeIcon icon={props.fai} className="text-3xl"/> */}
+          </div>
+          <div className="flex space-x-3 items-center">
+            <FontAwesomeIcon
+              icon={props.fai}
+              className="w-8 h-full align-middle justify-center text-2xl"
+            />
+            <p>{props.label}</p>
+          </div>
+        </div>
+        <span className="material-icons text-2xl">{props.mui}</span>
+      </div>
+    </Link>
+  );
 }
