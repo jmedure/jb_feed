c301a0bf134e32ffb44a0adcd6ba8a89a094c325
Jake <jacob@liveineverynow.com>
2023-05-14 12:15:22 -0400

    Vercel Analytics init

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,25 @@
-import Layout from '../components/Layout'
-import '../styles/globals.css'
-import '../styles/fonts.css'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
-import Script from "next/script";
+import Layout from '../components/Layout';
+import '../styles/globals.css';
+import '../styles/fonts.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+import Script from 'next/script';
+import { Analytics } from '@vercel/analytics/react';
 // import 'nextjs-breadcrumbs/dist/index.css';
 
-function MyApp({ Component, pageProps,}) {
+function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
-      <Script src="https://scripts.simpleanalyticscdn.com/latest.js"  />
-      <Script async src="https://scripts.simpleanalyticscdn.com/auto-events.js" />
+      <Analytics />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <Script
+        async
+        src="https://scripts.simpleanalyticscdn.com/auto-events.js"
+      />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
