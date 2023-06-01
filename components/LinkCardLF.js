a5381b767dfd68e8f51b85e21bc219440a1eb26d
Jake <jacob@liveineverynow.com>
2023-06-01 18:09:24 -0400

    LinkCardTest

--- components/LinkCardLF.js
+++ components/LinkCardLF.js
@@ -8,17 +8,24 @@ export default function LinkCardLF(props) {
       passHref
       href={props.href}
       className="hover:cursor-pointer"
-      legacyBehavior>
+      legacyBehavior
+    >
       <div className="py-4 pl-4 pr-4 w-full flex-grow text-2xl overflow-auto flex group hover:cursor-pointer bg-white/00 backdrop-blur hover:bg-white/10 justify-between border border-solid border-neutral-500 rounded-lg hover:-translate-y-1 transition-all hover:drop-shadow-2xl">
         <div className="flex items-center">
           <div className="">
             {/* <FontAwesomeIcon icon={props.fai} className="text-3xl"/> */}
           </div>
           <div className="flex space-x-3 items-center">
-            <FontAwesomeIcon
+            {`${props.fai}` ? (
+              <FontAwesomeIcon
+                icon={props.fai}
+                className="w-8 align-middle justify-center text-2xl"
+              />
+            ) : null}
+            {/* <FontAwesomeIcon
               icon={props.fai}
               className="w-8 align-middle justify-center text-2xl"
-            />
+            /> */}
             <p>{props.label}</p>
           </div>
         </div>
@@ -26,4 +33,4 @@ export default function LinkCardLF(props) {
       </div>
     </Link>
   );
-  }
+}
