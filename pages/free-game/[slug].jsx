312417f9482b5bd3a6e09da71c5a97b8a9f00439
Jake <jacob@liveineverynow.com>
2023-10-21 09:36:21 -0700

    silly

--- pages/free-game/[slug].jsx
+++ pages/free-game/[slug].jsx
@@ -1,64 +1,72 @@
-import dayjs from "dayjs"
-import React from "react"
-import Head from "next/head"
-import Image from "next/legacy/image"
-import rehypeSlug from "rehype-slug"
-import { MDXRemote } from "next-mdx-remote"
-import rehypeHighlight from "rehype-highlight"
-import rehypeCodeTitles from "rehype-code-titles"
-import { serialize } from "next-mdx-remote/serialize"
-import { getSlug, getArticleFromSlug } from '../../src/utils/mdx_fg'
+import dayjs from 'dayjs';
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/legacy/image';
+import rehypeSlug from 'rehype-slug';
+import { MDXRemote } from 'next-mdx-remote';
+import rehypeHighlight from 'rehype-highlight';
+import rehypeCodeTitles from 'rehype-code-titles';
+import { serialize } from 'next-mdx-remote/serialize';
+import { getSlug, getArticleFromSlug } from '../../src/utils/mdx_fg';
 // import "highlight.js/styles/atom-one-dark-reasonable.css"
-import rehypeAutolinkHeadings from "rehype-autolink-headings"
-import JBHeader from "../../components/MHeader"
-import Breadcrumbs from "nextjs-breadcrumbs"
-import Button from "../../components/Button"
-import FgEntry from "../../components/FgEntry"
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import JBHeader from '../../components/MHeader';
+// import Breadcrumbs from 'nextjs-breadcrumbs';
+import Button from '../../components/Button';
+import FgEntry from '../../components/FgEntry';
 
 export default function BlogPost({ post: { source, frontmatter } }) {
-  
   return (
     <React.Fragment>
       <Head>
         <title>{frontmatter.title} | My blog</title>
-        
       </Head>
       {/* <JBHeader/> */}
       <div className="container-blog prose-lg proseprose:neutral prose-headings:tracking-tighter prose-headings:font-serif prose-headings:font-medium py-20">
         <div className="not-prose flex-block font-sans border-b border-solid py-4 border-neutral-300">
-            <div className="">
-              {/* <Breadcrumbs useDefaultStyle omitRootLabel containerClassName="breadcrumbs" activeItemClassName="bread-crumb" /> */}
-              <Button href="./" type="left" copy=" back" icon="west"/>
-              <h1 className="text-6xl font-fruit tracking-tight mb-4 mt-20">{frontmatter.title}</h1>
-              <p className="text-2xl tracking-wide text-neutral-600">{frontmatter.description}</p>
+          <div className="">
+            {/* <Breadcrumbs useDefaultStyle omitRootLabel containerClassName="breadcrumbs" activeItemClassName="bread-crumb" /> */}
+            <Button href="./" type="left" copy=" back" icon="west" />
+            <h1 className="text-6xl font-fruit tracking-tight mb-4 mt-20">
+              {frontmatter.title}
+            </h1>
+            <p className="text-2xl tracking-wide text-neutral-600">
+              {frontmatter.description}
+            </p>
+          </div>
+          <div className="flex justify-betwee items-center tracking-wide text-neutral-500">
+            <div className="flex w-full align-middle space-x-4 not-prose my-4">
+              {/* <p className="publish-date">Published {dayjs(frontmatter.date).format("MMMM D, YYYY")} &mdash;{" "}</p> */}
+              <p className="">
+                Last edited{' '}
+                {dayjs(frontmatter.lastEdited).format('MMMM D, YYYY')}
+              </p>
+              <p>{frontmatter.readingTime}</p>
             </div>
-            <div className="flex justify-betwee items-center tracking-wide text-neutral-500">
-              <div className="flex w-full align-middle space-x-4 not-prose my-4">
-                {/* <p className="publish-date">Published {dayjs(frontmatter.date).format("MMMM D, YYYY")} &mdash;{" "}</p> */}
-                <p className="">Last edited {dayjs(frontmatter.lastEdited).format("MMMM D, YYYY")}</p>
-                <p>{frontmatter.readingTime}</p>
+            <div className="flex-col flex flex-shrink h-fill justify-items-end">
+              <div className="flex-row">
+                <span className="material-icons text-right h-auto text-xl border-2 px-2 text-neutral-400 border-neutral-200 border-solid hover:border-solid  hover:text-neutral-400 hover:border-2 hover:border-neutral-400 rounded-md transition-all ">
+                  ios_share
+                </span>
               </div>
-              <div className="flex-col flex flex-shrink h-fill justify-items-end">
-                      <div className="flex-row">
-                          <span className="material-icons text-right h-auto text-xl border-2 px-2 text-neutral-400 border-neutral-200 border-solid hover:border-solid  hover:text-neutral-400 hover:border-2 hover:border-neutral-400 rounded-md transition-all ">ios_share</span>
-                      </div>
-                  </div>
             </div>
           </div>
+        </div>
         <div className="relative w-full h-[40rem] border-solid border-neutral-300 border">
-              <Image
-                src={frontmatter.image}
-                alt={frontmatter.alt}
-                width={500}
-                height={500}
-                // placeholder="blur"
-                blurDataURL='https://jacobs.blue'
-                fill
-                sizes="100vw"
-                style={{
-                  objectFit: "cover"
-                }} />
-        </div> 
+          <Image
+            src={frontmatter.image}
+            alt={frontmatter.alt}
+            width={500}
+            height={500}
+            // placeholder="blur"
+            blurDataURL="https://jacobs.blue"
+            fill
+            sizes="100vw"
+            style={{
+              objectFit: 'cover',
+            }}
+          />
+        </div>
         <div className="content">
           <MDXRemote {...source} components={{ Image, Button, FgEntry }} />
         </div>
@@ -79,9 +87,9 @@ export async function getStaticProps({ params }) {
         [
           rehypeAutolinkHeadings,
           {
-            properties: { className: ["anchor"] },
+            properties: { className: ['anchor'] },
           },
-          { behaviour: "wrap" },
+          { behaviour: 'wrap' },
         ],
         rehypeHighlight,
         rehypeCodeTitles,
