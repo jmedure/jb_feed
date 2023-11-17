4edba33ebc572fbc96ea5e5eb9753065564bfbf3
Jake <jacob@liveineverynow.com>
2023-11-17 09:11:58 -0800

    Typos

--- components/mdx/CaseStudyMeta.jsx
+++ components/mdx/CaseStudyMeta.jsx
@@ -2,6 +2,10 @@ export default function CaseStudyMeta(props) {
   // const impacts = props.impact;
   return (
     <div className="not-prose flex flex-wrap gap-x-8 gap-y-7 text-base max-w-[600px] tracking-normal mx-auto">
+      <div className="white-space-nowrap space-y-2">
+        <p className="font-bold">Role</p>
+        <p>{props.role}</p>
+      </div>
       <div className="space-y-2 flex-wrap">
         <p className="font-bold">Impact</p>
         <p>{props.impact}</p>
@@ -14,10 +18,7 @@ export default function CaseStudyMeta(props) {
         <p className="font-bold">Timeline</p>
         <p>{props.timeline}</p>
       </div>
-      <div className="white-space-nowrap space-y-2">
-        <p className="font-bold">Role</p>
-        <p>{props.role}</p>
-      </div>
+
       {/* <div className="space-y-2">
         <p className="font-bold">Team</p>
         <p>{props.team}</p>
