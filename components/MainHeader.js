311a3dc1922c87ef8f75a03ca4779c2e54e7d7cc
Jake <jacob@liveineverynow.com>
2023-11-20 07:59:01 -0800

    No reload motion

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -81,14 +81,14 @@ export default function MainHeader(props) {
   return (
     <div className="flex-wrap bg-white sticky top-0 z-40">
       <motion.div
-        initial={{ opacity: 0, translateY: 10 }}
-        animate={{ opacity: 1, translateY: 0 }}
-        transition={{
-          type: 'spring',
-          delay: 0.1,
-          stiffness: 200,
-          duration: 0.5,
-        }}
+        // initial={{ opacity: 0, translateY: 10 }}
+        // animate={{ opacity: 1, translateY: 0 }}
+        // transition={{
+        //   type: 'spring',
+        //   delay: 0.1,
+        //   stiffness: 200,
+        //   duration: 0.5,
+        // }}
         className="container-fg py-3 w-full flex-row"
       >
         <div className="flex items-center justify-between w-full text-lg font-medium font-jbd">
